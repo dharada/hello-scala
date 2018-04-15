28bc8b9e0f3cc898b8d87a8d8b870c2af8ab73fd
dharada <dharada@guidewire.com>
2018-04-15 22:50:35 +0900

    update

--- src/main/scala/Point.scala
+++ src/main/scala/Point.scala
@@ -1,4 +1,4 @@
-class Point(var x: Int, var y: Int) {
+class Point(var x: Int, var y: Int = 0) {
 
   def move(dx: Int, dy: Int): Unit = {
     x = x + dx
@@ -8,5 +8,14 @@ class Point(var x: Int, var y: Int) {
   override def toString: String = {
     s"($x, $y)"
   }
+
+  //  基本コンストラクタ以外のコンストラクタを定義する場合は、 def this(...) で定義する。
+  //  補助コンストラクタは、必ず始めに基本コンストラクタを呼ばないといけない。
+  def this(str:String) {
+    this(x = 1)
+    println("after basic constructor" + x)
+  }
+
+
 }
 
